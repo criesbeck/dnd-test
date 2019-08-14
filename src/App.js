3ca7d2f1b1fa4547f22b00adc996917933354213
Chris Riesbeck <chris.riesbeck@gmail.com>
2019-08-14 13:15:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// https://github.com/asbjornenge/react-drag-and-drop
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';  
+import { Draggable, Droppable } from 'react-drag-and-drop'
 
-function App() {
+const CounterItem = ({type, data}) => (
+  <Draggable wrapperComponent={{ type: "button"}} type={type} data={data}>
+    {data}
+  </Draggable>
+);
+
+const inTarget = (e, position) => (
+  e.clientX >= position.left
+  && e.clientX <= position.right
+  && e.clientY >= position.top
+  && e.clientY <= position.bottom
+);
+
+const SmoothieItem = ({item, removeItem}) => {
+  const onDragEnd = (e) => {
+    if (!inTarget(e, item.target.current.position)) removeItem(item);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Draggable wrapperComponent={{ type: "button"}} 
+      type={item.type}
+      data={item.data}
+      onDragEnd={ onDragEnd }>
+      {item.data}
+    </Draggable>
   );
-}
+};
 
+const Smoothie = ({items, active, removeItem}) => (
+  <div style={{
+    border: `2px solid ${active ? 'red' : 'gray'}`,
+    height: '2em',
+    width: '20em'
+  }}>
+    {
+      items.map(item => <SmoothieItem key={item.ts} item={item} removeItem={removeItem} />)
+    }
+  </div>
+);
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [active, setActive] = useState(false);
+  const ref = useRef(null);
+
+  const addItem = (data, target) => {
+    const type = Object.keys(data)[0];
+    const item = { type: type, data: data[type], target: ref, ts: Date.now() };
+    setItems([ ...items, item ]);
+  };
+
+  const removeItem = (item) => {
+    setItems(items.filter(x => x !== item));
+  };
+
+  return (
+    <div>
+      <div>
+        <CounterItem type="fruit" data="Banana" />
+        <CounterItem type="fruit" data="Apple" />
+        <CounterItem type="utensil" data="Spoon" /> 
+      </div>
+      <Droppable ref={ref} types={['fruit']} 
+        onDrop={ (data, e) => { addItem(data); setActive(false); } }
+        onDragEnter={ () => setActive(true) }
+        onDragLeave={ () => setActive(false) }>
+        <Smoothie items={items} active={active} removeItem={removeItem} />
+      </Droppable>
+    </div>
+  )
+};
+
+Smoothie.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    data: PropTypes.string.isRequired,
+    ts: PropTypes.number.isRequired,
+  })).isRequired,
+  active: PropTypes.bool.isRequired,
+  removeItem: PropTypes.func.isRequired
+};
+  
 export default App;
+  
